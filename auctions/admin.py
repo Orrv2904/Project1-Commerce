02add99eaa07e1cd9a0a2139723ab38f674f5f65
Orrv2904 <Omarrv2904@gmail.com>
2023-08-15 23:50:28 -0600

    Revert to previous commit: <:bug: in the data base fixed>

--- auctions/admin.py
+++ auctions/admin.py
@@ -5,5 +5,3 @@ from .models import *
 admin.site.register(User)
 admin.site.register(Category)
 admin.site.register(Listing)
-admin.site.register(Comment)
-admin.site.register(Bid)
--- auctions/models.py
+++ auctions/models.py
@@ -5,13 +5,6 @@ from django.db import models
 class User(AbstractUser):
     pass
 
-class Bid(models.Model):
-    bid = models.FloatField(default=0)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="userBid")
-    
-    def __str__(self):
-        return self.bid
-
 class Category(models.Model):
     categoryName = models.CharField(max_length=50)
 
@@ -22,7 +15,7 @@ class Listing(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=100)
     imageUrl = models.ImageField(upload_to='images/commerce/', blank=True, default='default_image_url')
-    price = models.ForeignKey(Bid, on_delete=models.CASCADE, blank=True, null=True, related_name="bidPrice")
+    price = models.FloatField()
     isActive = models.BooleanField(default=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="user")
     created_at = models.DateTimeField(auto_now_add=True)
@@ -40,4 +33,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"{self.author} commented on {self.listing}"
-
